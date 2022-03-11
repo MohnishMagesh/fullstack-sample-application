1d4e5680788aa97097f1f543eae85dc1c58f06b6
MohnishMagesh <52004413+MohnishMagesh@users.noreply.github.com>
2022-03-10 21:06:36 -0700

    added edit post functionality

--- client/src/components/UserDetail.js
+++ client/src/components/UserDetail.js
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom';
 import PostComments from "./PostComments";
 import Albums from "./Albums";
 
+import { Form, Input } from 'antd';
+import { Modal } from 'antd';
 import { Button } from 'antd';
 import { Descriptions, Badge } from 'antd';
 import { Collapse } from 'antd';
@@ -38,9 +40,44 @@ export default function UserDetail(props) {
             )
     }, []);
 
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+    const showModal = () => {
+        setIsModalVisible(true);
+    };
+
+    const handleOk = () => {
+        setIsModalVisible(false);
+    };
+
+    const handleCancel = () => {
+        setIsModalVisible(false);
+    };
+
+    // crud operation
     const deletePost = (postID) => {
         setPostData(postData.filter(post => post.id !== postID));
         console.log(postData);
+    };
+
+    // crud operation
+    const updatePost = (values) => {
+        const postID = values.postid;
+        const title = values.title;
+        const body = values.body;
+
+        const updatedPost = {
+            "userId": userId,
+            "id": postID,
+            "title": title,
+            "body": body
+        }
+
+        setPostData(postData.map((post) => post.id === postID ? updatedPost : post));
+    };
+
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
     };
 
     return (
@@ -67,9 +104,54 @@ export default function UserDetail(props) {
                                     {/* create comments components here */}
                                     <PostComments postid={post.id}/>
                                     <div style={{marginTop: "10px"}}>
-                                        <Button type="primary" style={{marginRight: "10px"}}>
+                                        <Button onClick={showModal} type="primary" style={{marginRight: "10px"}}>
                                             Edit
                                         </Button>
+                                        <Modal title="Edit Post" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+                                            <Form
+                                                name="basic"
+                                                labelCol={{ span: 8 }}
+                                                wrapperCol={{ span: 16 }}
+                                                initialValues={{ remember: true }}
+                                                onFinish={updatePost}
+                                                onFinishFailed={onFinishFailed}
+                                                autoComplete="off"
+                                            >
+                                                <Form.Item
+                                                    label="PostID"
+                                                    name="postid"
+                                                    initialValue={post.id}
+                                                    rules={[{ required: true, message: 'Enter Title' }]}
+                                                    style={{display: "none"}}
+                                                >
+                                                    <Input />
+                                                </Form.Item>
+
+                                                <Form.Item
+                                                    label="Title"
+                                                    name="title"
+                                                    initialValue={post.title}
+                                                    rules={[{ required: true, message: 'Enter Title' }]}
+                                                >
+                                                    <Input />
+                                                </Form.Item>
+
+                                                <Form.Item
+                                                    label="Body"
+                                                    name="body"
+                                                    initialValue={post.body}
+                                                    rules={[{ required: true, message: 'Enter Post Details' }]}
+                                                >
+                                                    <Input.TextArea />
+                                                </Form.Item>
+
+                                                <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                                                    <Button type="primary" htmlType="submit">
+                                                        Edit post
+                                                    </Button>
+                                                </Form.Item>
+                                            </Form>
+                                        </Modal>
                                         <Button onClick={() => deletePost(post.id)} type="primary" danger>
                                             Delete
                                         </Button>
